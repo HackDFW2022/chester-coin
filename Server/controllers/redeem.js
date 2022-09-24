ed94d0c0761db5b93cc371c32ff361f2af284442
rahiblaghari <rlaghari@gmail.com>
2022-09-24 16:34:53 -0500

    fixed part of the backend

--- Server/controllers/redeem.js
+++ Server/controllers/redeem.js
@@ -5,13 +5,11 @@ import asyncHandler from 'express-async-handler';
 import blockchain from '../models/blockchain.js';
 
 const router = express.Router();
-// mongoose.set('useFindAndModify', false);
 
 export const postredeem = asyncHandler(async (req, res) => { 
     console.log(req.body.redeemCode)
-    if(await blockchain.findOneAndDelete({ "code" : req.body.redeemCode}).exec())
+    if(blockchain.findByIdAndDelete(req.body.redeemCode).exec())
     {
-        // blockchain.deleteOne({"code": req.body.redeemCode}).exec()
         loginMessage.findOneAndUpdate({"cryptoaccount": req.body.username}, {$inc : {'cryptoamount' : 1}}, {new:true}).exec()
         res.send(true)
     }
