35dd8c13b1d072502385bc1aad803a7da76df1c8
Emma <emmaxtung@gmail.com>
2022-09-24 14:08:09 -0500

    updated topbar logo and formatting

--- githubForFrontEndInitial/src/mainPageComponents/TopBar.js
+++ githubForFrontEndInitial/src/mainPageComponents/TopBar.js
@@ -1,10 +1,9 @@
 import React from "react";
-import logo from "../images/logo.png"
+import logo from "../images/chesterCheetoFullLogo.png"
 export default function TopBar(){
     return(
         <div className="bar">
             <img src={logo} className="barImage" alt="logo" height={100}/>
-            <span className="barText">Chester Portal</span> 
         </div>
     )
 }
