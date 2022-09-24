d5bf9b0ac2ec00acd7ebbe0e32b6db9ca66b1cad
Emma <emmaxtung@gmail.com>
2022-09-24 17:33:48 -0500

    added table skeleton code to trades page

--- githubForFrontEndInitial/src/mainPageComponents/differentDisplays/Trades.js
+++ githubForFrontEndInitial/src/mainPageComponents/differentDisplays/Trades.js
@@ -21,6 +21,31 @@ export default function MyEvents(input){
                     {getEvents(input.userinfo.events)}
                 </div>
             </div>
+
+            <div className="prizeTable center">
+                <table>
+                    <tr>
+                        <th>Prize</th>
+                        <th>Item</th>
+                        <th>Chester Coins</th>
+                    </tr>
+                    <tr>
+                        <td>picture</td>
+                        <td>iPad Mini</td>
+                        <td>500</td>
+                    </tr>
+                    <tr>
+                        <td>picture</td>
+                        <td>Gaming Laptop</td>
+                        <td>1000</td>
+                    </tr>
+                    <tr>
+                        <td>picture</td>
+                        <td>TV</td>
+                        <td>5000</td>
+                    </tr>
+                </table> 
+            </div>
         </div>
     )
 }
