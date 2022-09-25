2f09ab3c20be4aa5aaee2518cb488068cd83b72a
rahiblaghari <rlaghari@gmail.com>
2022-09-24 19:55:56 -0500

    nearing completion

--- githubForFrontEndInitial/src/mainPageComponents/differentDisplays/Redeem.js
+++ githubForFrontEndInitial/src/mainPageComponents/differentDisplays/Redeem.js
@@ -5,33 +5,26 @@ import cheetoBag from "./cheetoBag.png";
 
 let api = axios.create({
     baseURL: "http://localhost:5000/redeem",
-  })
+})
   
-  let config = {
-      headers: {
-         'Content-Type': 'application/json',
-      } 
-  }
+let config = {
+    headers: {
+        'Content-Type': 'application/json',
+    } 
+ }
 export default function Redeem(input){
     async function postCredentials(code){
-        // let getResponse = setTimeout(()=>api.post('/', {redeemCode: code, username:input.userinfo.cryptoaccount}, config), 1000)
         let getResponse = await api.post('/', {redeemCode: code, username:input.userinfo.cryptoaccount}, config)
         .then(Response=>Response.data)
         .catch()
-        //(console.log("Error, no backend server response, either backend not turned on or connection bad for some reason"))
         return getResponse
       }
 
     async function validateCode(){
         let redeemCode = document.getElementById("redeemCode").value
-        console.log(redeemCode)
-        // let success = await(postCredentials(redeemCode).value)
         let success = setTimeout(()=>postCredentials(redeemCode).value, 1000)
-        // let success = await(postCredentials(redeemCode).value)
-        console.log("Success: ")
-        console.log(success)
         if(success){
-            alert("Successfully redeemed, re-sign in to see your chester coins!")
+            alert("Coins may or may not have been redeemed, refresh and log in to find out!")
         }
         else{
             alert("Invalid redeem code, make sure everything was typed out correctly")
--- githubForFrontEndInitial/src/mainPageComponents/differentDisplays/Trades.js
+++ githubForFrontEndInitial/src/mainPageComponents/differentDisplays/Trades.js
@@ -1,24 +1,10 @@
 import React from "react";
 export default function MyEvents(input){
-
-    function printEvents(eventArray){
-        return eventArray.map(({dates, eventNames}) => <tr><td>{eventNames}</td> <td>{dates}</td></tr>)
-    }
-
-    function getEvents(eventArray){
-        if(!eventArray)
-            return "gotta make this a form to communicate to back end"
-        else {
-            return (<table><tr> <th>Event Names</th> <th>Date</th> </tr> {printEvents(eventArray)}</table>)
-        }
-    }
-
     return(
         <div>
             <div className="titleText">
                 Make a trade:
                 <div className="contentText">
-                    {getEvents(input.userinfo.events)}
                 </div>
             </div>
 
