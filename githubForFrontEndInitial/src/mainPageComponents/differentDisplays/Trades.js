60fda225f8705e900da551f5296bfaaaacb01410
anushka-vadde <anushka-vadde@users.noreply.github.com>
2022-09-24 20:53:02 -0500

    Reformated trading page

--- githubForFrontEndInitial/src/mainPageComponents/differentDisplays/Trades.js
+++ githubForFrontEndInitial/src/mainPageComponents/differentDisplays/Trades.js
@@ -1,34 +1,43 @@
 import React from "react";
-export default function MyEvents(input){
+import logo from "./logo.png"
+
+export default function MyTrades(input){
     return(
         <div>
             <div className="titleText">
-                Make a trade:
+                <u> <b>Trade for Some CHEDDER</b> </u>
                 <div className="contentText">
                 </div>
             </div>
-
+            <div className="titleText">
+                Current Balance: {input.userinfo.cryptoamount} Chester Coins 
+                <img src={logo} height={30} alt="logo" className="coin"/>
+            </div>
             <div className="prizeTable center">
                 <table>
                     <tr>
                         <th>Prize</th>
                         <th>Item</th>
                         <th>Chester Coins</th>
+                        <th> Buy? </th>
                     </tr>
                     <tr>
                         <td>picture</td>
                         <td>iPad Mini</td>
                         <td>500</td>
+                        <td> button</td>
                     </tr>
                     <tr>
                         <td>picture</td>
                         <td>Gaming Laptop</td>
                         <td>1000</td>
+                        <td> button </td>
                     </tr>
                     <tr>
                         <td>picture</td>
                         <td>TV</td>
                         <td>5000</td>
+                        <td> button </td>
                     </tr>
                 </table> 
             </div>
