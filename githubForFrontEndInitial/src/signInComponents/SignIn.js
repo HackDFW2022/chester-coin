3069f5fc5faaac989806ee1839fb359d67aa1f37
Emma <emmaxtung@gmail.com>
2022-09-24 21:27:01 -0500

    modified sign in page

--- githubForFrontEndInitial/src/signInComponents/SignIn.js
+++ githubForFrontEndInitial/src/signInComponents/SignIn.js
@@ -1,5 +1,8 @@
 import React from "react";
 import axios from 'axios';
+import steps from "../images/homePageSteps.png";
+import signIn from "../images/signInBox.png";
+import signUp from "../images/signUpBox.png";
 
 let api = axios.create({
   baseURL: "http://localhost:5000/validateSignIn",
@@ -33,15 +36,26 @@ export default function TopBar(input){
         }
     }
     return(
-        <div className="signInPage">
-            <div>Enter Username: </div>
-            <input type="username" id="username" maxLength="32" autoComplete="off">                
-            </input>
-            <div>Enter Password: </div>
-            <input type="password" id="password" maxLength="32" autoComplete="off">                
-            </input>
-            <br />
-            <button onClick={() => validateSignIn()}>Click to sign in</button>
+        <div>
+            <div className="signInPage float-parent">
+                <div className="float-child ">
+                    <b>Chester Rewards</b>
+                    <br />
+                    To become a Chester rewards member and start collecting Chester Coins, create or login to your 
+                    chestercoins.com account! You'll earn coins every time you make a Cheetos purchase - your 
+                    coins can be saved up or redeemed for Chester approved prizes!
+                </div>
+                <div className="float-child signInArea">
+                    <div>Enter Username:</div>
+                    <input type="username" id="username" maxLength="32" autoComplete="off" />
+                    <div>Enter Password: </div>
+                    <input type="password" id="password" maxLength="32" autoComplete="off" />                
+                    <br />
+                    <button> <img src={signIn} onClick={() => validateSignIn()}/></button>
+                    <br />
+                    <button> <img src={signUp}/></button>
+                </div>
+            </div>
         </div>
     )
 }
